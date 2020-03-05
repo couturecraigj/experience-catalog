7e6d32ed36f794789dc4452639552f89d2be0633
Craig Couture <couturecraigj@gmail.com>
2020-03-05 09:30:15 -0600

    Fix login button

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
+import DataService from "forcejs/dist/force.data-service";
 import Context from "./Context";
 
 const LoginButton = () => {
-  const [{ loggedIn, jsforce }, dispatch] = useContext(Context);
+  const [{ loggedIn, oAuth }, dispatch] = useContext(Context);
   const [rendered, setRendered] = useState(false);
   useEffect(() => {
     setRendered(true);
@@ -16,30 +17,43 @@ const LoginButton = () => {
       onClick={e => {
         window.onunload = () => {
           localStorage.removeItem("local_user");
-          jsforce.browser.logout();
         };
-        jsforce.browser.login(
-          {
-            loginUrl: window.loginUrl,
-            popup: { width: 800, height: 600 }
-          },
-          function(err) {
-            if (err) {
-              // eslint-disable-next-line no-console
-              console.error("Log in error : ", err);
-            }
-            jsforce.browser.connection
-              .identity()
-              .then(payload => {
-                dispatch({ type: "loggedin", payload });
-                // eslint-disable-next-line no-console
-                console.log("Log in successful");
-                localStorage.setItem("local_user", JSON.stringify(payload));
-              })
-              // eslint-disable-next-line no-console
-              .catch(console.error);
-          }
-        );
+
+        oAuth
+          .login()
+          .then(oauthResult =>
+            DataService.createInstance(oauthResult, { useProxy: false })
+          )
+          .then(() => {
+            const service = DataService.getInstance();
+            service.retrieve("User", service.getUserId()).then(response => {
+              dispatch({ type: "loggedin", payload: response });
+              localStorage.setItem("local_user", JSON.stringify(response));
+            });
+          });
+
+        // jsforce.browser.login(
+        //   {
+        //     loginUrl: window.loginUrl,
+        //     popup: { width: 800, height: 600 }
+        //   },
+        //   function(err) {
+        //     if (err) {
+        //       // eslint-disable-next-line no-console
+        //       console.error("Log in error : ", err);
+        //     }
+        //     jsforce.browser.connection
+        //       .identity()
+        //       .then(payload => {
+        //
+        //         // eslint-disable-next-line no-console
+        //         console.log("Log in successful");
+        //
+        //       })
+        //       // eslint-disable-next-line no-console
+        //       .catch(console.error);
+        //   }
+        // );
       }}
     >
       Login
