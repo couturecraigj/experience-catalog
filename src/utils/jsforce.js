8752ed4ea901b5723ba8afd1972b8e25afd53612
Craig Couture <couturecraigj@gmail.com>
2020-03-05 09:49:33 -0600

    improve performQuery

--- src/utils/jsforce.js
+++ src/utils/jsforce.js
@@ -1,13 +1,21 @@
-const performQuery = (jsforce, query) =>
-  new Promise((resolve, reject) => {
-    jsforce.browser.connection.query(query, (err, result) => {
-      if (err) return reject(err);
-      return resolve(result);
-    });
-  });
+import DataService from "forcejs/dist/force.data-service";
+
+const performQuery = (jsforce, query) => {
+  // return new Promise((resolve, reject) => {
+
+  //   jsforce.browser.connection.query(query, (err, result) => {
+  //     if (err) return reject(err);
+  //     return resolve(result);
+  //   });
+  // });
+  const service = DataService.getInstance();
+  if (!service) return;
+  return service.query(query);
+};
 
 const createSObject = (jsforce, objectName, record) =>
   new Promise((resolve, reject) => {
+    // const service = DataService.getInstance();
     jsforce.browser.connection
       .sobject(objectName)
       .create(record, (err, newRecord) => {
